0e83502596d5649a506e4a28543368a08f855550
codeMaster1983 <73346803+codeMaster1983@users.noreply.github.com>
2020-12-04 17:11:39 +0300

    Delete script.js

--- unit5/script.js
+++
@@ -1,5 +0,0 @@
-const btn = document.querySelector('.scr-sz');
-
-btn.addEventListener('click', () => {
-  alert(` Размеры области просмотра - ${window.innerWidth} на ${window.innerHeight} пикселей`)
-})
