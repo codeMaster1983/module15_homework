239e917f6070752bf38455a7eb0cce881aa9f020
codeMaster1983 <73346803+codeMaster1983@users.noreply.github.com>
2020-12-04 17:02:14 +0300

    Create script.js

---
+++ unit5/script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('.scr-sz');
+
+btn.addEventListener('click', () => {
+  alert(` Размеры области просмотра - ${window.innerWidth} на ${window.innerHeight} пикселей`)
+})
